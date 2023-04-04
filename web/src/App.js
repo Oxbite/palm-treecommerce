5a17c6684e34d8768443a3e0f91b49f9d46faa37
aoz69 <aozpokharel@gmail.com>
2023-04-04 12:22:13 +0545

    tweaks

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Nav />
       <Index />
-      {/* <Home /> */}
+      <Home />
     </div>
   );
 }
---
+++ web/src/components/homePage.jsx
@@ -0,0 +1,16 @@
+
+
+export default function homePage(){
+    return(
+        <div className="homepage">
+            <h1>Fire</h1>
+            <h1>Fire</h1>
+            <h1>Fire</h1>
+            <h1>Fire</h1>
+            <h1>Fire</h1>
+            <h1>Fire</h1>
+            <h1>Fire</h1>
+            <h1>Fire</h1>
+        </div>
+    )
+}
