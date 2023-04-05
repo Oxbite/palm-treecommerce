e71d8774a4b261d18a521bece7db0978af0ead97
aoz69 <aozpokharel@gmail.com>
2023-04-05 16:28:57 +0545

    tweaks

--- web/src/components/homePage.jsx
+++ web/src/components/homePage.jsx
@@ -2,7 +2,7 @@
 
 export default function homePage(){
     return(
-        <div className="homepage">
+        <div className="homepage ">
             <h1>Fire</h1>
             <h1>Fire</h1>
             <h1>Fire</h1>
--- web/src/components/index.jsx
+++ web/src/components/index.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.min.css'
 import ftProduct from '../image/lipStickc.svg';
-import arrow from '../image/arroow.svg';
+// import arrow from '../image/arroow.svg';
 import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper.min.css'
 
 
 export default function Home(){ 
-    const seasons = [ftProduct, ftProduct, ftProduct];
+    const seasons = [ftProduct, ftProduct, ftProduct,ftProduct];
 
     return(
 
         <>
-        <div className='homePage'>
+        <div className='homePage page'>
             <div className='cont'>
                 <div className='tagLine'>
                     <h1>NEW AND DARK</h1>
@@ -22,7 +22,7 @@ export default function Home(){
                     <button type="submit" className='btn'>ORDER NOW</button>
                 </div>
                 <div className='image'>
-                <object type="image/svg+xml" aria-label="pcImage" data={ftProduct}></object>
+                <object type="image/svg+xml" aria-label="pcImage" data={ftProduct} />
                 </div>
                 <div className='slider'>
                 <Swiper className='swiper_container'
@@ -32,24 +32,28 @@ export default function Home(){
                         "--swiper-pagination-bullet-inactive-opacity": "1",
                         "--swiper-pagination-bullet-size": "12px",
                         "--swiper-pagination-bullet-horizontal-gap": "4px",
-                        "--swiper-pagination-bottom": "20%"
+                        // "--swiper-pagination-bottom": "0%"
                     }}
                     modules={[Navigation, Pagination]}
-                    slidesPerView={1}
+                    slidesPerView={2}
                     navigation
                     pagination={{ clickable: true }}
                     scrollbar={{ draggable: true }}
                 >
                     {seasons.map((season)=>{
+                        console.log(season)
                         return(
                             <SwiperSlide>
                             <div className='imgSlider'>
-                                <object type="image/svg+xml" aria-label="pcImage" data={season} ></object>
+                                <div><object type="image/svg+xml" aria-label="pcImage" data={season} ></object></div>
                             </div>
                             </SwiperSlide>
                         )
                     })}
+                    
                 </Swiper>
+                {/* <object type="image/svg+xml" aria-label="pcImage" data={ftProduct} ></object> */}
+
                 </div>
                
             </div>
