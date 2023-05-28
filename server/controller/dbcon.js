cfa15338a13d2120746f621bea51c0040d4b6efb
aoz69 <38861986+aoz69@users.noreply.github.com>
2023-05-28 14:43:30 +0545

    Update dbcon.js

--- server/controller/dbcon.js
+++ server/controller/dbcon.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 
 exports.connect = async (req, res) => {
   await mongoose.connect(
-    "mongodb+srv://bliss:2eRYfCRdRuVMXi7M@woodland.pfprl.mongodb.net/?retryWrites=true&w=majority"
+    //CHANGE THIS!!!
+    "localhost:2740"
   );
   return true;
 };
